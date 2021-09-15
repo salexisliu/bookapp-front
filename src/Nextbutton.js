e12831b1990554dcd5f0ed2ec3e6a655a3faccf7
Alexis S Liu <syl1350@gmail.com>
2021-09-14 23:59:06 -0400

    sBookshelf css, Nextbutton component

---
+++ src/Nextbutton.js
@@ -0,0 +1,9 @@
+import React from "react";
+import Button from 'react-bootstrap/Button'
+
+function Nextbutton({ moreBooks}) {
+
+  return <Button onClick={moreBooks}>Next</Button>;
+}
+
+export default Nextbutton;
