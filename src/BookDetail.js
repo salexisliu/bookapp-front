9500e4912893a7cfa3c9bfabf17c9ccbd8708909
Samantha Laine <samanthalaine8@gmail.com>
2021-09-16 11:32:35 -0400

    changing covers

--- src/BookDetail.js
+++ src/BookDetail.js
@@ -3,18 +3,21 @@ import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import Card from "react-bootstrap/Card";
+import Button from "react-bootstrap/Button";
 // from app.js
 // link from userbook.js
 export default function BookDetail({ userbook }) {
   console.log(userbook);
   return (
     <Container>
-      <Card>
+      <Card style={{ width: "18rem" }}>
       <Col>{userbook.book.title}</Col>
-      <Col>userbook id:{userbook.id}</Col>
-      <Col>book id:{userbook.book_id}</Col>
+      <Col>Author: {userbook.book.author}</Col>
+      <Col>Page Count: {userbook.book.pages}</Col>
 
-      <img src={userbook.book.img_url}></img>
+      {/* <img src={userbook.book.img_url}></img> */}
+      <img src={'https://media.discordapp.net/attachments/887887430475186176/887887513539215480/image0.png?width=1285&height=885'}></img>
+      <Button variant="primary">Click to see more info</Button>
   </Card>
     </Container>
   );
